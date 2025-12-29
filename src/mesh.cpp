ab0c342a1af25f2ad93999ca16abd7d23fd6edd2
Ben McLemore <97567128+b-mclemore@users.noreply.github.com>
2025-12-29 10:06:08 -0500

    Add other platonic solids

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -7,14 +7,99 @@ Mesh::Mesh() {
     glGenVertexArrays(1, &VertexArrayID);
     glBindVertexArray(VertexArrayID);
 
-    GLfloat vertices[] = {
-        0.5f, -0.5f, 0.5f,
-        -0.5f, -0.5f, 0.5f,
-        0.5f, 0.5f, 0.5f,
-        -0.5f, 0.5f, 0.5f
-    };
+    // Tetrahedron
+    // GLfloat vertices[] = {
+    //     0.0f,  0.0f,  0.577f,
+    //     0.0f,  0.816f, -0.289f,
+    //    -0.707f, -0.408f, -0.289f,
+    //     0.707f, -0.408f, -0.289f
+    // };
+    // GLint triangles[] = {
+    //     0, 1, 2,
+    //     0, 3, 1,
+    //     0, 2, 3,
+    //     1, 3, 2
+    // };
 
-    GLint triangles[] = {0, 2, 3, 0, 3, 1};
+    // Cube
+    // GLfloat vertices[] = {
+    //     0.5f, -0.5f, 0.5f,
+    //     -0.5f, -0.5f, 0.5f,
+    //     0.5f, 0.5f, 0.5f,
+    //     -0.5f, 0.5f, 0.5f,
+    //     0.5f, -0.5f, -0.5f,
+    //     -0.5f, -0.5f, -0.5f,
+    //     0.5f, 0.5f, -0.5f,
+    //     -0.5f, 0.5f, -0.5f
+    // };
+    // GLint triangles[] = {0, 2, 3, 0, 3, 1,
+    //                       2, 6, 7, 2, 7, 3,
+    //                       6, 4, 5, 6, 5, 7,
+    //                       4, 0, 1, 4, 1, 5,
+    //                       1, 3, 7, 1, 7, 5,
+    //                       4, 6, 2, 4, 2, 0};
+
+    // Regular octahedron
+    // GLfloat vertices[] = {
+    //     -0.5f, 0.0f, 0.0f,
+    //     0.5f, 0.0f, 0.0f,
+    //     0.0f, 0.5f, 0.0f,
+    //     0.0f, -0.5f, 0.0f,
+    //     0.0f, 0.0f, 0.5f,
+    //     0.0f, 0.0f, -0.5f
+    // };
+    // GLint triangles[] = {
+    //     2, 0, 4,
+    //     2, 4, 1,
+    //     2, 1, 5,
+    //     2, 5, 0,
+    //     3, 4, 0,
+    //     3, 1, 4,
+    //     3, 5, 1,
+    //     3, 0, 5
+    // };
+
+    // Icosahedron
+    GLfloat phi = (1 + 2.2361) / 4;
+    GLfloat vertices[] = {
+        0.0f,  0.5f, phi,
+        0.0f,  0.5f, -phi,
+        0.0f,  -0.5f, phi,
+        0.0f,  -0.5f, -phi,
+        0.5f, phi, 0,
+        0.5f, -phi, 0,
+        -0.5f, phi, 0,
+        -0.5f, -phi, 0,
+        phi, 0, 0.5,
+        -phi, 0, 0.5,
+        phi, 0, -0.5,
+        -phi, 0, -0.5,
+    };
+    GLint triangles[] = {
+        // top
+        4, 0, 8,
+        0, 2, 8,
+        2, 5, 8,
+        5, 10, 8,
+        10, 4, 8,
+        // bottom
+        3, 7, 11,
+        7, 9, 11,
+        9, 6, 11,
+        6, 1, 11,
+        1, 3, 11,
+        // belt
+        3, 7, 5,
+        7, 9, 2,
+        9, 6, 0,
+        6, 1, 4,
+        1, 3, 10
+        // (other five faces of the belt are not added)
+    };
+    
+
+    // Dodecahedron
+    // (todo)
 
     glGenBuffers(1, &vertexbuffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
